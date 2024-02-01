5a2f073b72e3f4cf7376a837a73512b4bbb26b28
Chanikya K <157685732+Chanu716@users.noreply.github.com>
2024-02-01 14:27:30 +0530

    Add files via upload

---
+++ program_to_print_all_unique_elements_in_an_array..c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, j, Unique;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+      printf("Enter the %d element:",i);  
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Unique elements are :\n");
+    for (i = 0; i < n; i++) {
+        Unique = 1;
+        for (j = 0; j < i; j++) {
+            if (arr[i] == arr[j]) {
+                Unique = 0; 
+                break;
+            }
+        }
+        if (Unique) {
+            printf("%d ", arr[i]);
+        }
+    }
+
+    printf("\n");
+
+    return 0;
+}
